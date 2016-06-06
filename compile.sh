b39c2ebc14b365c181dc246ae6f409671b6df52d
Igor Nikolaev <igor.nikolaev@nikisoft.ru>
2016-06-06 22:30:03 +0300

    Modified that way JAVA_HOME is detected on Mac OS X

--- compile.sh
+++ compile.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-JAVA_VERSION=$(java -version 2>&1 | grep "java version" | tr -d "[:alpha:][:space:]\"")
-
-echo "Detected Java version ${JAVA_VERSION}"
+JAVA_HOME=$(/usr/libexec/java_home)
 
 mkdir -p target/lib
 
 gcc -o target/lib/libWKHtmlToPdfJavaBinding.jnilib \
   -lc -shared \
   -I./src/main/c/ \
-  -I/Library/Java/JavaVirtualMachines/jdk${JAVA_VERSION}.jdk/Contents/Home/include \
-  -I/Library/Java/JavaVirtualMachines/jdk${JAVA_VERSION}.jdk/Contents/Home/include/darwin \
+  -I${JAVA_HOME}/include \
+  -I${JAVA_HOME}/include/darwin \
   -I/usr/local/include/ \
   -L/usr/local/lib -lwkhtmltox \
   ./src/main/c/WKHtmlToPdfImpl.c
